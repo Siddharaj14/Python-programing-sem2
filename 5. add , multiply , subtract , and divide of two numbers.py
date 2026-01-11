68d6e5b2a3e514237bfd5c5d1da4550d84b60016
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2026-01-11 22:33:47 +0530

    Add files via upload

---
+++ 5. add , multiply , subtract , and divide of two numbers.py
@@ -0,0 +1,6 @@
+a = 5
+b = 3
+print("addition of two numbers is :",a+b)
+print("Multiplication of two numbers is :",a*b)
+print("subtraction of two numbers is :",a-b)
+print("divide of two numbers is :",a/b)
