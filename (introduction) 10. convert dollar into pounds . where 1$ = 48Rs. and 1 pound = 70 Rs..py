3ae951cef20e9ae68a5447fa4ee543e3dbfed3cb
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2026-02-21 13:14:05 +0530

    Delete (introduction) 10. convert dollar into pounds . where 1$ = 48Rs. and 1 pound = 70 Rs..py

--- (introduction) 10. convert dollar into pounds . where 1$ = 48Rs. and 1 pound = 70 Rs..py
+++
@@ -1,8 +0,0 @@
-def pounds() :
-    dollar = int(input("Enter dollars : "))
-    # 1 dollar = 48 rupees
-    # 1 pound = 70 rupees
-    pound = (70/48)*dollar
-    print("pounds is :",pound)
-
-pounds()
