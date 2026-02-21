01b3114792b80fc799aa6b167655aaef91259186
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2026-02-21 13:18:09 +0530

    Delete (introduction) 5. add , multiply , subtract , and divide two numbers.py

--- (introduction) 5. add , multiply , subtract , and divide two numbers.py
+++
@@ -1,9 +0,0 @@
-def all() :
-      a = int(input("Enter a number : "))
-      b = int(input("Enter another number : "))
-      print("addition is :",a+b)
-      print("subtraction is :",a-b)
-      print("multiplication is :",a*b)
-      print("divide is :",a/b)
-
-all()
