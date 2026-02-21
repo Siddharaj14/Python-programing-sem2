5a4664b0451fde1fb635256dc10591700bc5d30a
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2026-02-21 13:34:54 +0530

    Delete (decisions) 4. check whether a given number is divisible by 10 or not.py

--- (decisions) 4. check whether a given number is divisible by 10 or not.py
+++
@@ -1,9 +0,0 @@
-def divisible() :
-    a = int(input("Enter a number : "))
-
-    if a%10 == 0:
-        print("Given number is divisible by 10")
-    else:
-        print("Given number is not divisible by 10")
-
-divisible()
