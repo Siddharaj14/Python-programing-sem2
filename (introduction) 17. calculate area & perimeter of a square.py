ce2d32e4c919d0a5d9b3ac623b6f7920d3bd3700
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2026-02-21 13:15:44 +0530

    Delete (introduction) 17. calculate area & perimeter of a square.py

--- (introduction) 17. calculate area & perimeter of a square.py
+++
@@ -1,9 +0,0 @@
-def square() :
-    side = int(input("Enter side : "))
-    area = side*side
-    perimeter = 4*side
-
-    print("Area is :",area)
-    print("Perimeter is :",perimeter)
-
-square()
