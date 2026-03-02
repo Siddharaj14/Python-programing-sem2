08ac05e7b493985127af7badf223480d28c87d66
Siddharaj14 <chandavatsiddharajsinh@gmail.com>
2026-03-02 15:40:22 +0530

    Add files via upload

---
+++ Tuple/5. tuple.py
@@ -0,0 +1,10 @@
+import operator
+def tuple5() :
+    fooditems = (('Potato',30),()('Cabbage',40),(),()('Tomato',50),('Bhindi',100),())
+    newfooditems = []
+    for x in fooditems:
+        if x:
+            newfooditems.append(x)
+    print(fooditems,newfooditems,sep='\n\n')
+    
+tuple5()
